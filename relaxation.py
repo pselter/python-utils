d1a2813b977333e48d4fa4a34dc7a7db1ea6ed4f
pselter <ph.selter@gmail.com>
2019-06-19 18:42:05 -0700

    added S/N calc support

--- relaxation.py
+++ relaxation.py
@@ -49,7 +49,15 @@ def triple_expo(x,T1r_1,I1,T1r_2,I2,T1r_3,I3):
 def dual_expo(x,T1r_1,I1,T1r_2,I2):
     return I1*(np.exp(-1*x/T1r_1))+I2*(np.exp(-1*x/T1r_2))
 #-------------------------
+ 
+#-------------------------
+# define the fitfunction
+def dual_expo_test(x,T1r_1,I1,T1r_2):
+    I2=I1*2
+    return I1*(np.exp(-1*x/T1r_1))+I2*(np.exp(-1*x/T1r_2))
+#-------------------------
     
+
 #-------------------------
 # define the fitfunction
 def single_expo(x,T1r_1,I1):
@@ -146,6 +154,89 @@ class relaxation(object):
         self.path = path
         self.expno = expno
     #----------------------------------
+    
+    
+#    def importvdlist(self,listtype='vdlist'):
+#        """"Import the VPlist or VDlist and handle stuff like 1m to seconds conversion
+#            work in progress
+#        """
+#        if listtype == 'vplist':
+#            self.inlist = np.loadtxt(self.path+'/'+str(self.expno)+'/vplist')
+#        if listtype == 'vdlist':
+#            self.inlist = np.loadtxt(self.path+'/'+str(self.expno)+'/vdlist')
+#        
+#        
+#        
+#        return self.list
+    def getnoise(self,dat,noise_reg=(400,200)):
+        
+        self.noiseidx0 = (np.abs(self.xaxis - noise_reg[0])).argmin()
+        self.noiseidx1 = (np.abs(self.xaxis - noise_reg[1])).argmin()
+  
+        
+        self.np_noise = abs(self.noiseidx1-self.noiseidx0)
+        self.center = abs((self.noiseidx1-self.noiseidx0)*0.5)
+        self.ynoise = sum(dat[self.noiseidx0:self.noiseidx1])
+        self.y2noise = sum(dat[self.noiseidx0:self.noiseidx1]**2)
+        self.xynoise = 0
+        
+       
+        for n in range(1,int(self.center)):
+            self.xynoise+=n*(dat[int(self.center+n)]-dat[int(self.center-n)])
+        
+        self.gnoise = np.sqrt((self.y2noise - (self.ynoise**2 + 3*self.xynoise**2/(self.np_noise**2-1))/self.np_noise)/(self.np_noise-1))
+        print(self.gnoise)
+        return self.gnoise
+
+
+
+    def T1rho2(self,start_procno,npoints,region=(20,-10),noise_reg=(400,200), normalize=True):
+        """Function to do the integration, returns the integral and time in an array         
+        written with T1_rho measurements by spin-locking in mind
+        
+        assumes a vplist being present
+        - start_procno :the first procno with slices
+        - npoints      :the number of procnos to evaluate
+        - region       :the region to integrate in ppm
+        - normalize    :normalizes the data to 100
+        """
+        
+        self.vplist = np.loadtxt(self.path+'/'+str(self.expno)+'/vplist')
+        self.intensity = np.zeros(npoints)
+        self.noise = np.zeros(npoints)
+        self.sn = np.zeros(npoints)
+        self.data = np.zeros((npoints,3))
+
+        
+        #----------------------------------
+        #Do the integration over all procnos
+        for n in range(npoints):
+            spectrum = bruk.bruker1d(self.path,self.expno,procno=start_procno+n)
+            self.x, self.y = spectrum.plot1d()
+            self.xaxis = np.array(self.x)
+            
+            
+            self.idx0 = (np.abs(self.xaxis - region[0])).argmin()
+            self.idx1 = (np.abs(self.xaxis - region[1])).argmin()
+            self.intensity[n] = np.amax(self.y[self.idx0:self.idx1])
+            self.noise[n]=self.getnoise(self.y)
+            self.sn[n]=self.intensity[n]/(2*self.noise[n])
+        #----------------------------------
+        
+        #----------------------------------
+        # Check for normalization
+        if normalize == True:
+            self.norm_int = self.intensity/self.intensity.max()*100
+            self.data[:,0] = self.vplist
+            self.data[:,1] = self.norm_int
+            self.data[:,2] = 1/self.sn
+        else:
+            self.data[:,0] = self.vplist
+            self.data[:,1] = self.intensity
+            self.data[:,2] = 1/self.sn
+        #----------------------------------
+        return self.data
+
 
     
     def T1rho(self,start_procno,npoints,region=(20,-10), normalize=True):
@@ -291,8 +382,82 @@ class relaxation(object):
             self.Int[1] = self.popt[3]
             self.T2[2] = self.popt[4]
             self.Int[2] = self.popt[5]
+            
+        if n_expo == 99:
+            self.popt, self.pcov = curve_fit(dual_expo_test,self.data[:,0],self.data[:,1],maxfev=5000,p0=(1.0e-2,33,1.0e-3))
+            self.fit[:,1] = dual_expo_test(self.time,*self.popt)
+            self.res[:,1] = self.data[:,1]-dual_expo_test(self.data[:,0],*self.popt)
+            
+            self.errors = np.sqrt(np.diagonal(self.pcov))
+            self.T2[0] = self.popt[0]
+            self.Int[0] = self.popt[1]
+            self.T2[1] = self.popt[2]
+#            self.Int[1] = self.popt[3]
 
         return self.fit, self.res, self.T2, self.Int, self.errors
+
+
+
+    def exp_decay2(self,data,n_expo=1):
+        """ Function to fit an exponential decay of your data
+        
+        - data : array with the times and intensities
+        - n_expo : 1-3, how many exponential decays to fit
+        
+        returns the fit, the residual, and the results
+        """
+        
+        self.data = data
+        self.npoints = len(data[:,0])
+        self.time = np.arange(0,self.data[-1,0],self.data[0,0])
+       
+        self.res = np.zeros((self.npoints,2))
+        self.fit =  np.zeros((len(self.time),2))
+        
+        self.errors = np.zeros(n_expo)
+        self.T2 = np.zeros(n_expo)
+        self.Int = np.zeros(n_expo)
+        
+        self.fit[:,0] = self.time
+        self.res[:,0] = self.data[:,0]
+        
+        if n_expo == 1:
+            self.popt, self.pcov = curve_fit(single_expo,self.data[:,0],self.data[:,1],sigma=self.data[:,2],maxfev=5000,p0=(1.0e-2,100))
+            self.fit[:,1] = single_expo(self.time,*self.popt)
+            self.res[:,1] = self.data[:,1]-single_expo(self.data[:,0],*self.popt)
+            
+            self.errors = np.sqrt(np.diagonal(self.pcov))
+            self.T2 = self.popt[0]
+            self.Int = self.popt[1]
+    
+        if n_expo == 2:
+            self.popt, self.pcov = curve_fit(dual_expo,self.data[:,0],self.data[:,1],sigma=self.data[:,2],maxfev=5000,p0=(1.0e-2,50,1.0e-3,50))
+            self.fit[:,1] = dual_expo(self.time,*self.popt)
+            self.res[:,1] = self.data[:,1]-dual_expo(self.data[:,0],*self.popt)
+            
+            self.errors = np.sqrt(np.diagonal(self.pcov))
+            self.T2[0] = self.popt[0]
+            self.Int[0] = self.popt[1]
+            self.T2[1] = self.popt[2]
+            self.Int[1] = self.popt[3]
+        
+        if n_expo == 3:
+            self.popt, self.pcov = curve_fit(triple_expo,self.data[:,0],self.data[:,1],sigma=self.data[:,2],maxfev=5000,p0=(5.0e-2,33,1.0e-2,33,5.0e-3,33))
+            self.fit[:,1] = triple_expo(self.time,*self.popt)
+            self.res[:,1] = self.data[:,1]-triple_expo(self.data[:,0],*self.popt)
+
+            self.errors = np.sqrt(np.diagonal(self.pcov))
+            self.T2[0] = self.popt[0]
+            self.Int[0] = self.popt[1]
+            self.T2[1] = self.popt[2]
+            self.Int[1] = self.popt[3]
+            self.T2[2] = self.popt[4]
+            self.Int[2] = self.popt[5]
+
+        return self.fit, self.res, self.T2, self.Int, self.errors
+
+
+
 
     def sat_rec(self,data,n_expo=1):
         """ Function to fit saturation recovery experiments
