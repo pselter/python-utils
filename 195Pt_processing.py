a0e17eb77805fe8015cd93a9c3cd94b3fd933b08
pselter <ph.selter@gmail.com>
2020-03-06 14:30:45 -0800

    added the sinc function to the cpmg things

--- 195Pt_processing.py
+++ 195Pt_processing.py
@@ -12,28 +12,31 @@ import matplotlib.pyplot as plt
 from scipy.fftpack import fft,ifft
 from scipy.fftpack import fftshift
 
-# path = 'C:/Users/Selter/OneDrive/old_sciebo_share/NMR-Rohdaten/NMR300/p_selt01/nmr/LOH102-2'
-path = 'C:/Users/philipp/OneDrive/old_sciebo_share/NMR-Rohdaten/NMR300/p_selt01/nmr/LOH-64&65'
+# Add your data path here
+path = 'C:/Users/Selter/OneDrive/old_sciebo_share/NMR-Rohdaten/NMR300/p_selt01/nmr/LOH-64&65'
 
-gauss = cpmg.create_gauss(npoints=402,width=10,center=180)
+# define the gaussian bell
+gauss = cpmg.create_gauss(npoints=402,width=2,center=150)
+
+gauss = cpmg.create_sinc(npoints=402,width=50,center=170)
+# concatenated the gaussian bells
 multigauss = cpmg.create_rp_wdw(0,gauss,150,0)
 
 
 test1 = cpmg.cpmg_data(path,expno=20)
 time, data = test1.return_fid()
 test1.apply_wdw(multigauss)
+
 time, data2 = test1.return_fid()
 freq, fftdata = test1.return_fft(64*1024,32*1024)
-
-
 
 
 fig = plt.figure()
 plt.plot(data/data.max()*0.5)
 plt.plot(multigauss)
 plt.plot(data2/data2.max()*0.5)
+plt.xlim(0,300)
 plt.show()
-
 
 
 fig = plt.figure()
@@ -42,18 +45,21 @@ plt.xlim(freq[-1],freq[0])
 plt.show()
 
 
+fig = plt.figure()
+sumtest = np.zeros(64*1024)
+for k in range(0,200,5):
+    gauss = cpmg.create_sinc(npoints=402,width=30,center=50+k)
+    # concatenated the gaussian bells
+    multigauss = cpmg.create_rp_wdw(0,gauss,150,0)
+    test1.apply_wdw(multigauss)
+    freq, fftdata = test1.return_fft(64*1024,32*1024)
+    sumtest = sumtest + fftdata
+    test1.reload_fid()
+    plt.plot(freq, np.abs(fftdata))
+plt.xlim(freq[-1],freq[0])
+plt.show()
 
-# fig = plt.figure()
-# # (0,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150)
-
-# for this in (np.arange(0,150,10)):
-#     gauss = cpmg.create_gauss(npoints=402,width=15,center=150+this)
-#     print(100+this)
-#     multigauss = cpmg.create_rp_wdw(0,gauss,100,0)
-#     test1.apply_wdw(multigauss)
-#     freq, data = test1.return_fft(128*1024)
-#     plt.plot(freq, np.abs(data))
-#     test1.reload_fid()
-
-# plt.xlim(freq[-1],freq[0])
-# plt.show()
+fig = plt.figure()
+plt.plot(freq, np.abs(sumtest))
+plt.xlim(freq[-1],freq[0])
+plt.show()
--- cpmg_utils.py
+++ cpmg_utils.py
@@ -12,14 +12,65 @@ from scipy.fftpack import fft,ifft
 from scipy.fftpack import fftshift
 import math
 
-
+def create_sinc(npoints=100,width=10,center=50):
+    
+    points = np.arange(npoints)
+    out = np.zeros(npoints)
+    for n in points:
+        if n == center:
+            out[n] = 1
+        else:
+            out[n]= np.sin(width/100*(n-center))/(width/100*(n-center))
+    return out
+    
 
 def create_gauss(npoints=100,width=10,center=50):
+    """  
+    Creates a Gaussian window function
+
+    Parameters
+    ----------
+    npoints : INTEGER, optional
+        number of points in the window. The default is 100.
+    width : INTEGER, optional
+        FWHH in points. The default is 10.
+    center : INTEGER, optional
+        where the center of the bell is. The default is 50.
+
+    Returns
+    -------
+    out : list of floats
+        returns a window function, in this case the gaussian bell.
+
+    """
     points = np.arange(npoints)
     out = np.exp(-0.5*((points-center)/(width))**2)
     return out
 
 def create_rp_wdw(front,wdw,nwdw,back):
+    """
+    Repeats a window function by concatenating it
+    Optionally adds zeros front and back
+
+    Parameters
+    ----------
+    front : INTEGER
+        zeros to be added to the front.
+    wdw : LIST of FLOATS
+        previously created window function.
+    nwdw : INTEGER
+        number of wdw to be concatenated.
+    back : INTEGER
+        number of zeros to be added at the end.
+
+    Returns
+    -------
+    out : LIST of FLOATS
+        Concatenated list.
+
+    """
+    
+    
     first = np.zeros(front)
     last = np.zeros(back)
     out = np.concatenate((first, wdw))
@@ -31,6 +82,21 @@ def create_rp_wdw(front,wdw,nwdw,back):
 class cpmg_data(object):
     
     def __init__(self,path,expno=1):
+        """
+        Creates the cpmg_data handling object
+
+        Parameters
+        ----------
+        path : STRING
+            path to the bruker data set.
+        expno : INTEGER, optional
+            EXPNO of the experiment. The default is 1.
+
+        Returns
+        -------
+        None.
+
+        """
         print('cpmg object created')
         self.dpath = path+'/'+str(expno)
         self.dict, self.data = ng.bruker.read(self.dpath)
@@ -41,21 +107,74 @@ class cpmg_data(object):
         self.time_axis = np.arange(0,self.AQ,2*self.DW)
         
     def reload_fid(self):
+        """
+        Reloads the raw FID for processing
+        Useful if something went wrong
+
+        Returns
+        -------
+        None.
+
+        """
+        
         print('loaded original FID')
         self.time_axis = np.arange(0,self.TD*2*self.DW,2*self.DW)
         self.dict, self.data = ng.bruker.read(self.dpath)
 
     def apply_em(self,lb):
+        """
+        Applies exponential linebroadening
+
+        Parameters
+        ----------
+        lb : FLOAT or INTEGER
+            LB constant analog to LB in Topspin.
+
+        Returns
+        -------
+        None.
+
+        """
         print('EM linebroadeing applied')
         for n in range(len(self.data)):
             self.data[n] = self.data[n]*np.exp(-((n-1)*lb*np.pi)/(2.0*self.SWH))
         
     def apply_gm(self,gb,lb):
+        """
+         Applies gaussian linebroadening
+
+        Parameters
+        ----------
+        gb : FLOAT
+            Value between 0 and 1, shift of the gaussian bell
+            
+        lb : FLOAT
+            negative value describing the width of the bell curve, higher means narrower curve.
+
+        Returns
+        -------
+        None.
+
+        """
         print('GM linebroadeing applied')
         self.b = lb/(2*gb*self.time_axis[-1])        
         self.data = self.data*np.exp((-1.0*lb*self.time_axis)-(-1.0*self.b*self.time_axis**2))
     
     def apply_wdw(self,wdw):
+        """
+        Multiplies each point of the FID with the value given in WDW
+
+        Parameters
+        ----------
+        wdw : List of floats
+            window function, must be as long as data set.
+
+        Returns
+        -------
+        None.
+
+        """
+        
         self.rest = np.zeros(len(self.data)-len(wdw))
         self.wdw = np.concatenate((wdw, self.rest))
         self.data=self.data*self.wdw
